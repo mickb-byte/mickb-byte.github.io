668a3ff3dc195312334e60874b943199d2a57cec
mickb-byte <mickiwiki@outlook.com>
2024-05-23 20:23:41 -0400

    Adding wiki style

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log('Welcome to my portfolio!');
+/* js/main.js */
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav ul li a');
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            console.log(`${link.textContent} link clicked`);
+        });
+    });
+});
